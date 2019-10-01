5c1ed46bb1ac04e99f6cb9cc750565f833eb4f21
Khozema Nullwala <khozema.nullwala@accenture.com>
2019-10-01 20:00:57 +0530

    Fixed the Shell Script!

--- tasks_script.sh
+++ tasks_script.sh
@@ -4,10 +4,10 @@ set -ex
 
 output_dir=workspace
 
-mvn -Dmaven.repo.local=$(pwd)/spring-petclinic/cache -DskipTests -f $(pwd)/spring-petclinic/pom.xml package
+mvn -Dmaven.repo.local=$(pwd)/code/cache -DskipTests -f $(pwd)/code/pom.xml package
 
-mv $(pwd)/spring-petclinic/target/*.jar "${output_dir}/"
+mv $(pwd)/code/target/*.jar "${output_dir}/"
 
-cp $(pwd)/spring-petclinic-concourse/docker/Dockerfile "${output_dir}/"
+cp $(pwd)/task-code/docker/Dockerfile "${output_dir}/"
 
 ls -alR "${output_dir}"
